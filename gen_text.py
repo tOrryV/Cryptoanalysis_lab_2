e03f6e4fc8eb8446b0e992a71bbd4198ba78a1ce
Viktoria <96387123+tOrryV@users.noreply.github.com>
2025-10-04 21:56:06 +0300

    Implemented function for generate random  text

---
+++ gen_text.py
@@ -0,0 +1,13 @@
+import random
+
+
+def generate_random_text(_alphabet, text_len):
+    """
+    Random text generator.
+    Generates a string of specified length using a uniform distribution of characters.
+    :param _alphabet: Alphabet (string or list of characters) to sample from
+    :param text_len: Desired length of the generated text (positive integer)
+    :return: Randomly generated text with uniform character distribution
+    """
+
+    return ''.join(random.choice(_alphabet) for _ in range(text_len))

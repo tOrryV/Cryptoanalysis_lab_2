d6bae1bb15a49c654623af5dd504a41031519c12
Viktoria <96387123+tOrryV@users.noreply.github.com>
2025-10-04 22:12:04 +0300

    Implemented function for recursive generate text

--- gen_text.py
+++ gen_text.py
@@ -11,3 +11,24 @@ def generate_random_text(_alphabet, text_len):
     """
 
     return ''.join(random.choice(_alphabet) for _ in range(text_len))
+
+
+def generate_recurse_text(_alphabet, text_len):
+    """
+    Generate recursive text where:
+      s0, s1 — random,
+      s_i = (s_{i-1} + s_{i-2}) mod len(alphabet)
+    :param _alphabet: list or string of characters
+    :param text_len: desired length (>=2)
+    """
+    if text_len <= 0:
+        return ''
+
+    rand = random.SystemRandom()
+    res = rand.choice(_alphabet) + rand.choice(_alphabet)
+
+    for _ in range(text_len - 2):
+        idx = (_alphabet.index(res[-1]) + _alphabet.index(res[-2])) % len(_alphabet)
+        res += _alphabet[idx]
+
+    return res

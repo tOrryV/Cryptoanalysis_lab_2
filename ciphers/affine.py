7087ad88591168116863154b106d28c781952e7e
Viktoria <96387123+tOrryV@users.noreply.github.com>
2025-10-19 19:08:46 +0300

    Delete non using functions

--- ciphers/affine.py
+++ ciphers/affine.py
@@ -21,29 +21,3 @@ def encrypt(_alphabet, _text, a, b):
         y = (a * x + b) % m
         result += _alphabet[y]
     return result
-
-
-def decrypt(_alphabet, _text, a, b):
-    """
-    Affine cipher decryption.
-    Formula: D(y) = a_inv * (y - b) mod m
-    :param _alphabet: Alphabet (string or list of characters)
-    :param _text: Ciphertext to decrypt (characters must be in _alphabet)
-    :param a: Multiplicative key 'a' (must be coprime with the alphabet size m)
-    :param b: Additive key 'b' (an integer in [0, m-1])
-    :return: Decrypted text (plaintext)
-    """
-
-    m = len(_alphabet)
-    if eae(a, m)[0] != 1:
-        raise ValueError(f"'a'={a} must be coprime with the alphabet length m={m}")
-    a_inv = eae(a, m)[1] % m
-
-    result = ''
-    for ch in _text:
-        y = _alphabet.index(ch)
-        x = (a_inv * (y - b)) % m
-        result += _alphabet[x]
-    return result
-
-
--- ciphers/vigenere.py
+++ ciphers/vigenere.py
@@ -17,22 +17,3 @@ def encrypt(_alphabet, _text, _key):
         result += _alphabet[value]
 
     return result
-
-
-def decrypt(_alphabet, _text, _key):
-    """
-    The decrypt function takes in a message and key, and returns the decrypted message.
-    :param _alphabet: Define the alphabet used in the encryption and decryption
-    :param _text: Store the text to be decrypted
-    :param _key: Decrypt the text
-    :return: A string of the decrypted text
-    """
-
-    result = ''
-    for i in range(len(_text)):
-        symbol_index = _alphabet.index(_text[i])
-        key_index = _alphabet.index(_key[i % len(_key)])
-        value = (symbol_index - key_index) % len(_alphabet)
-        result += _alphabet[value]
-
-    return result
